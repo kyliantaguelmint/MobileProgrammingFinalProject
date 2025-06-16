2584969ed77eda6232482064ccfa76ff6325a1c3
edgeSky <irulannaba@gmail.com>
2025-06-17 01:11:36 +0700

    add splash screen and launcher icon

--- lib/screens/article_detail_page.dart
+++ lib/screens/article_detail_page.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase/services/comment_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase/database/article.dart';
 
 class ArticleDetailPage extends StatelessWidget {
   final String articleId;
